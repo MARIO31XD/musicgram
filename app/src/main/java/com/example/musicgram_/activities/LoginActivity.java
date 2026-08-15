df3364dfaeb28e2bf52acc0419e87fddf1feb4ee
mariopiquemartinez <mapi155@vidalibarraquer.net>
2026-08-15 12:42:22 +0200

    Comprobación en el login, if Correo y contraseña estan vacios

--- app/src/main/java/com/example/musicgram_/activities/LoginActivity.java
+++ app/src/main/java/com/example/musicgram_/activities/LoginActivity.java
@@ -9,6 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.musicgram_.R;
 
@@ -18,18 +19,30 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login); // activity_login.xml es la interfaz donde podremos hacer login
 
         // fields of form
-        EditText etEmail = findViewById(R.id.etEmail); // field email
-        EditText etPassword = findViewById(R.id.etPassword); // field Password
-
         Button btnLogin = findViewById(R.id.btnLogin); // boton para hacer Login y iniciar sesión
 
+        EditText etEmail = findViewById(R.id.etEmail); // field email variable etEmail
+        EditText etPassword = findViewById(R.id.etPassword); // field Password variable etPassword
 
+        // Logica cuando hagamos Click en el btnLogin
         btnLogin.setOnClickListener(v ->{
+
           String email = etEmail.getText().toString(); // cogemos los datos de los Strings
           String password = etPassword.getText().toString(); // tanto del email como del password
+
+            // if el email está vacio // y la contraseña // SINO HACES EL LOGIN CORRECTAMENTE
+        if(email.isEmpty()) {
+            Toast.makeText(this, "Introduce tu correo electrónico", Toast.LENGTH_SHORT).show();
+
+        } else if(password.isEmpty()) {
+            Toast.makeText(this,"Introduce tu contraseña", Toast.LENGTH_SHORT).show();
+        } else{
+             Toast.makeText(this, "Login correcto", Toast.LENGTH_SHORT).show();
+            }
+
         });
 
     }
