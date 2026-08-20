cb20427cfc25cd83ca69d94463eaec4a8a4c07cf
mariopiquemartinez <mapi155@vidalibarraquer.net>
2026-08-20 14:49:46 +0200

    Comprovacion validaciones register screen

--- app/src/main/java/com/example/musicgram_/activities/RegisterActivity.java
+++ app/src/main/java/com/example/musicgram_/activities/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.musicgram_.activities;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,28 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register); // xml de Activity Register
+
+        // Los valores EditText que hay que rellenar en el Register
+        EditText etUsername = findViewById(R.id.etUsername);
+        EditText etEmail = findViewById(R.id.etEmail);
+        EditText etPassword = findViewById(R.id.etPassword);
+        EditText etConfirmPassword = findViewById(R.id.etConfirmPassword);
+
+
+        String username = etUsername.getText().toString();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String confirmPassword = etConfirmPassword.getText().toString();
+
+        // validaciones
+        if (username.isEmpty()) {
+            Toast.makeText(this, "Introduce tu Nombre de Usuario", Toast.LENGTH_SHORT).show();
+        }
+         else if (email.isEmpty()) {
+            Toast.makeText(this, "Introduce tu Email", Toast.LENGTH_SHORT).show();
+        }
+
+
 
 
     }
