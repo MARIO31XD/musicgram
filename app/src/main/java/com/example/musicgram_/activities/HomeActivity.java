5bf9495a3381ef3cc2a2b03609967b074867ef84
mariopiquemartinez <mapi155@vidalibarraquer.net>
2026-08-29 18:12:03 +0200

    Creación de la HomeActivity (diseño provisional) y conectar esta con SearchActivity y ProfileActivity

--- app/src/main/java/com/example/musicgram_/activities/HomeActivity.java
+++ app/src/main/java/com/example/musicgram_/activities/HomeActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
+import android.widget.Button;
 import com.example.musicgram_.R;
 
 public class HomeActivity extends AppCompatActivity {
@@ -17,6 +19,26 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
+        // BUTTONS DEL XML
+        Button btnHome = findViewById(R.id.btnHome);
+        Button btnSearch = findViewById(R.id.btnSearch);
+        Button btnProfile = findViewById(R.id.btnProfile);
+
+        // BUTTON SEARCH cuando se haga click abrirá el SearchActivity
+        btnSearch.setOnClickListener(v -> {
+            // para abrir la SearchActivity
+            Intent intent = new Intent(HomeActivity.this, SearchActivity.class);
+            startActivity(intent);
+
+        });
+
+        // BUTTON PROFILE PARA CUANDO SE HAGA CLICK
+        btnProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+            startActivity(intent);
+
+        });
 
     }
 }
