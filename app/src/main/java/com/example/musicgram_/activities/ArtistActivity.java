0f6c7119d7b32e0fbe942e8611f1fca1a1ead69e
mariopiquemartinez <mapi155@vidalibarraquer.net>
2026-09-15 18:50:28 +0200

    Added new properties in activity_artist and photos of artists // ArtistActivity updates

--- app/src/main/java/com/example/musicgram_/activities/ArtistActivity.java
+++ app/src/main/java/com/example/musicgram_/activities/ArtistActivity.java
@@ -1,6 +1,7 @@
 package com.example.musicgram_.activities;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -22,8 +23,87 @@ public class ArtistActivity extends AppCompatActivity {
         String artistName = getIntent().getStringExtra("artist");
 
         TextView txtArtistName = findViewById(R.id.txtArtistName);
+        TextView txtArtistBiography = findViewById(R.id.txtArtistBiography);
+        ImageView imgArtist = findViewById(R.id.imgArtist);
+
+        TextView txtMonthlyListeners = findViewById(R.id.txtMonthlyListeners); // oyentes mensuales
+
+        // SONGS
+        TextView txtSong1 = findViewById(R.id.txtSong1);
+        TextView txtSong2 = findViewById(R.id.txtSong2);
+        TextView txtSong3 = findViewById(R.id.txtSong3);
+
+        // IMAGES AND TEXT OF ALBUMS
+        ImageView imgAlbum1 = findViewById(R.id.imgAlbum1);
+        ImageView imgAlbum2 = findViewById(R.id.imgAlbum2);
+
+        TextView txtAlbum1 = findViewById(R.id.txtAlbum1);
+        TextView txtAlbum2 = findViewById(R.id.txtAlbum2);
+
         txtArtistName.setText(artistName);
 
+        // comprovación del Artista
+        if(artistName.equals("The Weeknd")) {
+            imgArtist.setImageResource(R.drawable.weeknd); // la ruta donde está la foto del artista
+
+            txtMonthlyListeners.setText("Más de 100 M de oyentes mensuales"); // oyentes mensuales
+
+            // songs
+            txtSong1.setText("▶  Blinding Lights");
+            txtSong2.setText("▶  Save Your Tears");
+            txtSong3.setText("▶  Starboy");
+
+            // Albums + images
+            txtAlbum1.setText("After Hours");
+            txtAlbum2.setText("Dawn FM");
+
+            imgAlbum1.setImageResource(R.drawable.after_hours); // image del album
+            imgAlbum2.setImageResource(R.drawable.dawnfm);
+
+
+            txtArtistBiography.setText( // text para la bio
+
+                    "The Weeknd took over pop music & culture on his own terms filtering R&B, Pop,& hip-hop through an ambitious widescreen lens. The multi-platinum 4X GRAMMY Award winner has emerged as one of the most successful & significant artists of the modern era."
+            );
+        }
+        else if (artistName.equals("The Strokes")) {
+
+            imgArtist.setImageResource(R.drawable.thestrokes);
+            txtMonthlyListeners.setText("Más de 17 M de oyentes mensuales");
+
+            txtSong1.setText("▶  Reptilia");
+            txtSong2.setText("▶  Last Nite");
+            txtSong3.setText("▶  Someday");
+
+            txtAlbum1.setText("Is this it");
+            txtAlbum2.setText("The New Abnormal");
+
+            imgAlbum1.setImageResource(R.drawable.isthisit);
+            imgAlbum2.setImageResource(R.drawable.strokes);
+
+            txtArtistBiography.setText(
+                    "By bringing style, fun, and catchy songwriting back to rock music, the Strokes became one of the most influential bands of the early 21st century."
+            );
+        }
+        else if (artistName.equals("Ariana Grande")) {
+
+            imgArtist.setImageResource(R.drawable.ariana);
+            txtMonthlyListeners.setText("Más de 100 M de oyentes mensuales");
+
+            txtSong1.setText("▶  7 rings");
+            txtSong2.setText("▶  thank u, next");
+            txtSong3.setText("▶  Into You");
+
+            txtAlbum1.setText("Thank u, Next");
+            txtAlbum2.setText("Positions");
+
+            imgAlbum1.setImageResource(R.drawable.thanku);
+            imgAlbum2.setImageResource(R.drawable.positions);
+
+            txtArtistBiography.setText(
+                    "With her four-octave vocal acrobatics and iconic image, American singer, songwriter, and actress Ariana Grande has solidified her place as one of the quintessential pop stars of her generation, racking up stacks of awards and chart records in the process."
+            );
+        }
 
     }
 }
