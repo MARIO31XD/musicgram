95cb53497e41f3c9eeffead2c60bd7b3c44bcc7a
mariopiquemartinez <mapi155@vidalibarraquer.net>
2026-09-18 12:12:02 +0200

    AlbumActivity con su portada y songs + activity_song con la lista de canciones de cada album

--- app/src/main/java/com/example/musicgram_/activities/AlbumActivity.java
+++ app/src/main/java/com/example/musicgram_/activities/AlbumActivity.java
@@ -1,6 +1,9 @@
 package com.example.musicgram_.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -21,7 +24,172 @@ public class AlbumActivity extends AppCompatActivity {
 
         String albumName = getIntent().getStringExtra("album");
         String artistName = getIntent().getStringExtra("artist");
+
+        LinearLayout songsContainer = findViewById(R.id.songsContainer);
+        ImageView imgAlbumCover = findViewById(R.id.imgAlbumCover);
         TextView txtAlbumName = findViewById(R.id.txtAlbumName);
+        TextView txtAlbumArtist = findViewById(R.id.txtAlbumArtist);
+        TextView txtAlbumYear = findViewById(R.id.txtAlbumYear);
+
         txtAlbumName.setText(albumName);
+        txtAlbumArtist.setText(artistName);
+
+        String [] songs = new String [0]; // Array de Songs (lista  inicial vacia)
+
+        // comprovaciones de portada de Album
+        if (albumName.equals("After Hours")) {
+
+            imgAlbumCover.setImageResource(R.drawable.after_hours);
+            txtAlbumYear.setText("2020");
+
+            // Array de texto con las songs de AFTER HOURS
+            songs  = new String[]  {
+                    "Alone Again",
+                    "Too Late",
+                    "Hardest To Love",
+                    "Scared To Live",
+                    "Snowchild",
+                    "Escape From LA",
+                    "Heartless",
+                    "Faith",
+                    "Blinding Lights",
+                    "In Your Eyes",
+                    "Save Your Tears",
+                    "After Hours",
+                    "Until I Bleed Out"
+
+            };
+
+        } else if (albumName.equals("Dawn FM")) {
+
+            imgAlbumCover.setImageResource(R.drawable.dawnfm);
+            txtAlbumYear.setText("2022");
+
+            songs =  new String[] {
+                    "Dawn FM",
+                    "Gasoline",
+                    "How Do I Make You Love Me?",
+                    "Take My Breath",
+                    "Sacrifice",
+                    "A Tale by Quincy",
+                    "Out of Time",
+                    "Here We Go... Again",
+                    "Best Friends",
+                    "Is There Someone Else?",
+                    "Starry Eyes",
+                    "Every Angel Is Terrifying",
+                    "Don't Break My Heart",
+                    "I Heard You're Married",
+                    "Less Than Zero",
+                    "Phantom Regret by Jim"
+
+            };
+
+        } else if (albumName.equals("Is This It")) {
+
+            imgAlbumCover.setImageResource(R.drawable.isthisit);
+            txtAlbumYear.setText("2001");
+
+            songs =  new String[] {
+                    "Is This It",
+                    "The Modern Age",
+                    "Soma",
+                    "Barely Legal",
+                    "Someday",
+                    "Alone, Together",
+                    "Last Nite",
+                    "Hard to Explain",
+                    "New York City Cops",
+                    "Trying Your Luck",
+                    "Take It or Leave It"
+
+            };
+
+
+        } else if (albumName.equals("The New Abnormal")) {
+
+            imgAlbumCover.setImageResource(R.drawable.strokes);
+            txtAlbumYear.setText("2020");
+
+            songs = new String[] {
+                    "The Adults Are Talking",
+                    "Selfless",
+                    "Brooklyn Bridge to Chorus",
+                    "Bad Decisions",
+                    "Eternal Summer",
+                    "At the Door",
+                    "Why Are Sundays So Depressing",
+                    "Not the Same Anymore",
+                    "Ode to the Mets"
+
+            };
+
+        } else if (albumName.equals("Thank U, Next")) {
+
+            imgAlbumCover.setImageResource(R.drawable.thanku);
+            txtAlbumYear.setText("2019");
+
+            songs = new String[]{
+                    "Imagine",
+                    "Needy",
+                    "NASA",
+                    "Bloodline",
+                    "Fake Smile",
+                    "Bad Idea",
+                    "Make Up",
+                    "Ghostin",
+                    "In My Head",
+                    "7 Rings",
+                    "Thank U, Next",
+                    "Break Up with Your Girlfriend, I'm Bored"
+            };
+
+        } else if (albumName.equals("Positions")) {
+
+            imgAlbumCover.setImageResource(R.drawable.positions);
+            txtAlbumYear.setText("2020");
+
+            songs = new String[]{
+                    "Shut Up",
+                    "34+35",
+                    "Motive",
+                    "Just Like Magic",
+                    "Off the Table",
+                    "Six Thirty",
+                    "Safety Net",
+                    "My Hair",
+                    "Nasty",
+                    "West Side",
+                    "Love Language",
+                    "Positions",
+                    "Obvious",
+                    "POV"
+            };
+        }
+        // recorre la Array de songs (cada song)
+        for (String song : songs) {
+
+            TextView songText = new TextView(this); // crea un texto con cada cancion
+
+
+            songText.setText("▶  " + song); // name de la song
+            songText.setTextSize(17);
+            songText.setPadding(10, 15, 10, 15);
+
+            // al pulsar una Song
+            songText.setOnClickListener(v -> {
+                Intent intent = new Intent(AlbumActivity.this , SongActivity.class);
+                intent.putExtra("song", song);
+                intent.putExtra("album", albumName);
+                intent.putExtra("artist", artistName);
+                startActivity(intent);
+
+            });
+
+            songsContainer.addView(songText); // se añade en el container que hemos creado en el activity_Album
+
+        }
+
+
     }
 }
